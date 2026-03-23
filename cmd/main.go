a725b0122df17c8d2a8b14e3de905277cdb62731
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-23 18:08:52 +0100

    added argument parsing

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/Petroviiic/GoGit/internal/commands"
 )
 
 func main() {
-	args := os.Args
-	if len(args) > 1 {
-		if args[1] == "init" {
-			fmt.Println("gitinit")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: gogit <command>")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	args := os.Args[2:]
+
+	switch command {
+	case "init":
+		if err := commands.RunInit(args); err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
 		}
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		os.Exit(1)
 	}
 
 }
