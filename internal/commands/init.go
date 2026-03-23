1d71bb7a9eadb1aee04d0160278bdc94e04c71cd
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-03-23 18:09:07 +0100

    added support for git init

---
+++ internal/commands/init.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/Petroviiic/GoGit/internal/core"
+)
+
+func RunInit(args []string) error {
+	path := "."
+	if len(args) > 0 {
+		path = args[0]
+	}
+
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		return err
+	}
+
+	repo := core.NewRepository(absPath)
+	err = repo.Init()
+	if err != nil {
+		// if errors.Is(err, fs.ErrExist){
+		// 	return fmt.Errorf("")
+		// }
+		return fmt.Errorf("failed to initialize repository: %w", err)
+	}
+
+	fmt.Printf("Initialized empty GoGit repository in %s\n", repo.GitDir)
+	return nil
+}
--- internal/core/repository.go
+++ internal/core/repository.go
@@ -1,1 +1,52 @@
 package core
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Repository struct {
+	WorkTree string
+	GitDir   string
+}
+
+func NewRepository(path string) *Repository {
+	return &Repository{
+		WorkTree: path,
+		GitDir:   filepath.Join(path, ".gogit"),
+	}
+}
+
+func (r *Repository) Init() error {
+	fmt.Println(r.GitDir, r.WorkTree)
+
+	dirs := []string{
+		r.GitDir,
+		filepath.Join(r.GitDir, "objects"),
+		filepath.Join(r.GitDir, "refs"),
+		filepath.Join(r.GitDir, "refs/dirs"),
+	}
+
+	if _, err := os.Stat(r.GitDir); !os.IsNotExist(err) {
+		return fmt.Errorf("repository at %s already exists", r.GitDir)
+	}
+
+	for _, dir := range dirs {
+		err := os.MkdirAll(dir, 0755)
+		if err != nil {
+			return err
+		}
+	}
+
+	headPath := filepath.Join(r.GitDir, "HEAD")
+	if err := os.WriteFile(headPath, []byte("ref: refs/heads/main\n"), 0644); err != nil {
+		return err
+	}
+
+	indexPath := filepath.Join(r.GitDir, "index")
+	if err := os.WriteFile(indexPath, []byte(""), 0644); err != nil {
+		return err
+	}
+	return nil
+}
