2ef2934a440ce783d7a2d6a17586ea63dddacb74
MisterDodik <drazenpetrovic56@gmail.com>
2026-04-20 01:00:48 +0200

    added add directory

--- internal/commands/add.go
+++ internal/commands/add.go
@@ -1,10 +1,12 @@
 package commands
 
 import (
-	"encoding/json"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"slices"
+	"strings"
 
 	"github.com/Petroviiic/GoGit/internal/core"
 )
@@ -17,9 +19,12 @@ func RunAdd(args []string, repo *core.Repository) error {
 }
 
 func addPath(path string, repo *core.Repository) error {
-	fullPath := filepath.Join(repo.WorkTree, path)
+	index, err := repo.LoadIndex()
+	if err != nil {
+		return err
+	}
 
-	fmt.Println(fullPath, path, repo.GitDir, repo.WorkTree)
+	fullPath := filepath.Join(repo.WorkTree, path)
 	fileInfo, err := os.Stat(fullPath)
 	if err != nil {
 		if os.IsNotExist(err) {
@@ -29,18 +34,48 @@ func addPath(path string, repo *core.Repository) error {
 	}
 
 	if fileInfo.IsDir() {
-		return addDirectory(path, repo)
+		err = addDirectory(path, repo, index)
 	} else {
-		return addFile(path, repo)
+		err = addFile(path, repo, index)
 	}
-}
 
-func addDirectory(path string, repo *core.Repository) error {
+	if err != nil {
+		return err
+	}
+
+	err = repo.SaveIndex(index)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
-func addFile(path string, repo *core.Repository) error {
+func addDirectory(path string, repo *core.Repository, index map[string]string) error {
+	var files []string
+	filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return err
+	})
+
+	addedCount := 0
+	for _, file := range files {
+		if slices.Contains(strings.Split(filepath.ToSlash(file), "/"), ".gogit") {
+			continue
+		}
+		if err := addFile(file, repo, index); err != nil {
+			return err
+		}
+		addedCount++
+	}
+
+	fmt.Printf("Added %d files from directory %s", addedCount, path)
+	return nil
+}
+
+func addFile(path string, repo *core.Repository, index map[string]string) error {
 	//reading the file
 	content, err := os.ReadFile(path)
 	if err != nil {
@@ -57,28 +92,12 @@ func addFile(path string, repo *core.Repository) error {
 	}
 
 	//updating index
-	indexRaw, err := os.ReadFile(repo.IndexPath)
-	if err != nil {
-		return err
-	}
-	//reading index
-	var index map[string]string
-	err = json.Unmarshal(indexRaw, &index)
-	if err != nil {
-		return err
-	}
-	//updating index
-	index[path] = blob_hash[2:]
+	// relPath, err := filepath.Rel(repo.WorkTree, path)
+	// if err != nil {
+	// 	return err
+	// }
+	index[path] = blob_hash
 
-	newIndex, err := json.MarshalIndent(index, "", "  ")
-	if err != nil {
-		return err
-	}
-	//saving changes
-	err = os.WriteFile(repo.IndexPath, newIndex, 0644)
-	if err != nil {
-		return err
-	}
 	return nil
 }
 
