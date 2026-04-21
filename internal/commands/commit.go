88584516777f79f9ef936b5858cd1215fd484a87
MisterDodik <drazenpetrovic56@gmail.com>
2026-04-21 22:19:28 +0200

    Update commit.go

--- internal/commands/commit.go
+++ internal/commands/commit.go
@@ -8,5 +8,19 @@ import (
 
 func RunCommit(repo *core.Repository, message, author string) error {
 	fmt.Println(message, author)
+	index, err := repo.LoadIndex()
+
+	if err != nil {
+		return err
+	}
+
+	hierarchyRoot := core.CreateFolderHierarchy(index)
+
+	treeHash, err := core.CreateTreeStructure(hierarchyRoot, repo)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(treeHash)
 	return nil
 }
