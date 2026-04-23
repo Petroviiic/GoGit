f4605fa8ae6c48628062e6a66774831fdf0c8a41
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-04-23 02:53:38 +0200

    Construct Commit object in RunCommit
    Update RunCommit to build a Commit object instead of constructing a tree and printing it. Index loading and tree creation calls are commented out for now; the current branch and its commit are retrieved to populate parent hashes. A new commit is instantiated with author, message, a placeholder treeHash and current UTC timestamp. Debug/serialization prints remain commented; commit is not yet persisted. This prepares commit metadata assembly while deferring index/tree creation and final storage.

--- internal/commands/commit.go
+++ internal/commands/commit.go
@@ -2,25 +2,53 @@ package commands
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/Petroviiic/GoGit/internal/core"
 )
 
 func RunCommit(repo *core.Repository, message, author string) error {
 	fmt.Println(message, author)
-	index, err := repo.LoadIndex()
+	// index, err := repo.LoadIndex()
 
-	if err != nil {
-		return err
-	}
+	// if err != nil {
+	// 	return err
+	// }
 
-	hierarchyRoot := core.CreateFolderHierarchy(index)
+	// hierarchyRoot := core.CreateFolderHierarchy(index)
 
-	treeHash, err := core.CreateTreeStructure(hierarchyRoot, repo)
-	if err != nil {
-		return err
-	}
+	// treeHash, err := core.CreateTreeStructure(hierarchyRoot, repo)
+	// if err != nil {
+	// 	return err
+	// }
+	//fmt.Println(treeHash)
 
-	fmt.Println(treeHash)
+	currentBranch := repo.GetCurrentBranch()
+	branchCommit := repo.GetBranchCommit(currentBranch)
+	parentHashes := []string{branchCommit}
+
+	commit := core.NewCommit(
+		author,
+		author,
+		message,
+		"treeHash",
+		parentHashes,
+		time.Now().UTC(),
+	)
+	_ = commit
+
+	// deserialized, _ := core.ParseCommit(commit.Content)
+
+	// fmt.Println(
+	// 	"printam",
+	// 	deserialized.Author,
+	// 	deserialized.Committer,
+	// 	deserialized.Message,
+	// 	deserialized.ParentHashes,
+	// 	deserialized.Timestamp,
+	// 	string(deserialized.BaseObject.Content),
+	// )
+	//fmt.Println(commit.Serialize())
+
 	return nil
 }
