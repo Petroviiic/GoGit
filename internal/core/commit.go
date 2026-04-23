514981886e218015a97e6be31b26009a18cb55d9
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-04-23 02:53:28 +0200

    Add Commit type with serialization
    Replace simple Commit fields with a richer struct (ParentHashes, Author, Committer, Message, TreeHash, Timestamp) and implement NewCommit constructor. NewCommit sets BaseObject.Type to "commit", populates fields, serializes the commit into the internal content format and assigns it to BaseObject.Content. serialize() emits a git-like commit body (tree, parent lines for each parent, author, committer with Unix timestamp +0000, blank line, then message). ParentHash was changed to ParentHashes to support merge commits.

--- internal/core/commit.go
+++ internal/core/commit.go
@@ -1,24 +1,58 @@
 package core
 
+import (
+	"fmt"
+	"strings"
+	"time"
+)
+
 type Commit struct {
 	BaseObject
-	ParentHash string // Dodatno polje koje Blob nema
-	Author     string
+	ParentHashes []string
+	Author       string
+	Committer    string
+	Message      string
+	TreeHash     string
+	Timestamp    time.Time
 }
 
-// func NewObject(objectType string, content []byte) *Object {
-// 	return &Object{
+func NewCommit(author, committer, message, treeHash string, parentHashes []string, timeStamp time.Time) *Commit {
+	commit := &Commit{
+		BaseObject: BaseObject{
+			Type: "commit",
+		},
+		ParentHashes: parentHashes,
+		Author:       author,
+		Committer:    committer,
+		Message:      message,
+		TreeHash:     treeHash,
+		Timestamp:    timeStamp,
+	}
 
-// 		deserialize: func(compressedData []byte) {
-// 			b := bytes.NewReader(compressedData)
+	content := commit.serialize()
 
-// 			r, err := zlib.NewReader(b)
-// 			if err != nil {
-// 				panic(err)
-// 			}
-// 			defer r.Close()
+	commit.BaseObject.Content = content
 
-// 			io.Copy(os.Stdout, r)
-// 		},
-// 	}
-// }
+	return commit
+}
+
+func (c *Commit) serialize() []byte {
+	//tree <tree_hash>
+	//parent <parent_hash>
+	//author <name> <timestamp> <timezone>
+	//committer <name> <timestamp> <timezone>
+	//
+	//<message>
+
+	lines := []string{fmt.Sprintf("tree %s", c.TreeHash)}
+	for _, parentHash := range c.ParentHashes {
+		lines = append(lines, fmt.Sprintf("parent %s", parentHash))
+	}
+
+	lines = append(lines, fmt.Sprintf("author %s %v +0000", c.Author, c.Timestamp.Unix()))
+	lines = append(lines, fmt.Sprintf("committer %s %v +0000", c.Committer, c.Timestamp.Unix()))
+	lines = append(lines, "")
+	lines = append(lines, c.Message)
+
+	return []byte(strings.Join(lines, "\n"))
+}
