b6b24cbf1c75f946636123627e8635f90858fe72
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-05-04 23:06:57 +0200

    Implement status: compare worktree, index, commit
    Re-enable and implement the repository status command. Walks the working tree (excluding .git/.gogit), creates blob hashes for files, and loads the current index and latest commit tree. Compares working directory vs index to list untracked and "changes not staged" (modified/deleted), and compares index vs last commit to list staged changes (new/modified/deleted). Adds required imports and prints results (includes some debug prints). Uses core.NewBlob, repo.LoadIndex, repo.LoadObject and core.GetIndexFromTreeHash.

--- internal/commands/status.go
+++ internal/commands/status.go
@@ -1,62 +1,151 @@
 package commands
 
 import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
 	"github.com/Petroviiic/GoGit/internal/core"
 )
 
 func RunStatus(repo *core.Repository) error {
-	// currentBranch := repo.GetCurrentBranch()
-	// fmt.Printf("On branch %s\n", currentBranch)
+	currentBranch := repo.GetCurrentBranch()
+	fmt.Printf("On branch %s\n", currentBranch)
 
-	// workingDirFiles := make(map[string]string)
-	// if err := filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
-	// 	if err != nil {
-	// 		return err
-	// 	}
+	workingDirFiles := make(map[string]string)
+	if err := filepath.WalkDir(".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
 
-	// 	parts := strings.Split(filepath.ToSlash(path), "/")
-	// 	if slices.Contains(parts, ".git") || slices.Contains(parts, ".gogit") {
-	// 		return nil
-	// 	}
+		parts := strings.Split(filepath.ToSlash(path), "/")
+		if slices.Contains(parts, ".git") || slices.Contains(parts, ".gogit") {
+			return nil
+		}
 
-	// 	fullPath := filepath.Join(repo.WorkTree, path)
+		fullPath := filepath.Join(repo.WorkTree, path)
 
-	// 	info, err := os.Stat(fullPath)
-	// 	if info.IsDir() {
-	// 		return err
-	// 	}
+		info, err := os.Stat(fullPath)
+		if info.IsDir() {
+			return err
+		}
 
-	// 	content, err := os.ReadFile(fullPath)
+		content, err := os.ReadFile(fullPath)
 
-	// 	blob := core.NewBlob(content)
-	// 	workingDirFiles[path] = blob.Hash()
+		blob := core.NewBlob(content)
+		workingDirFiles[filepath.ToSlash(path)] = blob.Hash()
 
-	// 	return err
-	// }); err != nil {
-	// 	return err
-	// }
-	// //fmt.Println(workingDirFiles, len(workingDirFiles))
+		return err
+	}); err != nil {
+		return err
+	}
+	fmt.Println("\nworking directory files", workingDirFiles, len(workingDirFiles))
 
-	// latestCommit := repo.GetBranchCommit(currentBranch)
-	// lastCommitFiles := make(map[string]string)
+	latestCommit := repo.GetBranchCommit(currentBranch)
+	lastCommitFiles := make(map[string]string)
 
-	// obj, err := repo.LoadObject(latestCommit)
-	// if err != nil {
-	// 	return err
-	// }
+	obj, err := repo.LoadObject(latestCommit)
+	if err != nil {
+		return err
+	}
 
-	// commit := obj.(*core.Commit)
-	// if commit.TreeHash != "" {
-	// 	if err := core.GetIndexFromTreeHash(commit.TreeHash, repo, ".", lastCommitFiles); err != nil {
-	// 		return err
-	// 	}
-	// }
-	// // fmt.Println(lastCommitFiles, len(lastCommitFiles))
+	commit := obj.(*core.Commit)
+	if commit.TreeHash != "" {
+		if err := core.GetIndexFromTreeHash(commit.TreeHash, repo, ".", lastCommitFiles); err != nil {
+			return err
+		}
+	}
+	fmt.Println("\nlast commit files", lastCommitFiles, len(lastCommitFiles))
 
-	// index, err := repo.LoadIndex()
-	// if err != nil {
-	// 	return err
-	// }
+	index, err := repo.LoadIndex()
+	if err != nil {
+		return err
+	}
+	fmt.Println("\nindex", index)
+
+	//UNTRACKED
+	untracked := []string{} //in working dir and not in index
+	for path := range workingDirFiles {
+		if _, ok := index[path]; !ok {
+			untracked = append(untracked, path)
+		}
+	}
+
+	if len(untracked) > 0 {
+		fmt.Printf("\nUntracked files:\n\t(use 'go run ./... add <file>...' to include in what will be committed)\n")
+		for _, file := range untracked {
+			fmt.Printf("\t\t%s\n", file)
+		}
+	}
+
+	//UNSTAGED 	disk vs index					//Changes not staged for commit
+	//  modifed (present in index and working dir and hashes are different)
+	// 	deleted (in index and not in working dir)
+	unstagedModifed := []string{}
+	unstagedDeleted := []string{}
+
+	for path, indexEntry := range index {
+		hash, ok := workingDirFiles[path]
+		if !ok {
+			unstagedDeleted = append(unstagedDeleted, path)
+		} else {
+			if indexEntry.Hash != hash {
+				unstagedModifed = append(unstagedModifed, path)
+			}
+		}
+	}
+
+	if len(unstagedModifed) > 0 || len(unstagedDeleted) > 0 {
+		fmt.Printf("\nChanges not staged for commit:\n\t(use 'git add <file>...' to update what will be committed)\n")
+		for _, file := range unstagedModifed {
+			fmt.Printf("\t\tmodified:\t%s\n", file)
+		}
+		for _, file := range unstagedDeleted {
+			fmt.Printf("\t\tdeleted:\t%s\n", file)
+		}
+	}
+
+	//STAGED	index vs last commit 		//Changes to be committed
+	//	new (in index and not in the last commit)
+	//  modifed (present in index and last commit and hashes are different)
+	// 	deleted (present in the last commit and not in index)
+	stagedNew := []string{}
+	stagedModifed := []string{}
+	stagedDeleted := []string{}
+
+	for path, indexEntry := range index {
+		hash, ok := lastCommitFiles[path]
+		if !ok {
+			stagedNew = append(stagedNew, path)
+		} else {
+			if indexEntry.Hash != hash {
+				stagedModifed = append(stagedModifed, path)
+			}
+		}
+	}
+	for path := range lastCommitFiles {
+		if _, ok := index[path]; !ok {
+			stagedDeleted = append(stagedDeleted, path)
+		}
+	}
+
+	if len(stagedModifed) > 0 || len(stagedDeleted) > 0 || len(stagedNew) > 0 {
+		fmt.Printf("\nChanges staged for commit:\n")
+		for _, file := range stagedNew {
+			fmt.Printf("\t\tnew:\t%s\n", file)
+		}
+		for _, file := range stagedModifed {
+			fmt.Printf("\t\tmodified:\t%s\n", file)
+		}
+		for _, file := range stagedDeleted {
+			fmt.Printf("\t\tdeleted:\t%s\n", file)
+		}
+	}
+
+	//OLD IMPLEMENTATION
 
 	// stagedModified := []string{}
 	// stagedNew := []string{}
