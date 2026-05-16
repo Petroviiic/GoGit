bf90deae28dfc6733c35c295ccd5d24a910f0a89
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-05-17 01:16:08 +0200

    Update abortMerge.go

--- internal/commands/abortMerge.go
+++ internal/commands/abortMerge.go
@@ -8,5 +8,9 @@ import (
 
 func AbortMerge(repo *core.Repository) error {
 	fmt.Println("aborting merge")
+
+	//check if merge head exists
+	//remove merge head
+	//restore working dir and index to head.lastcommit
 	return nil
 }
