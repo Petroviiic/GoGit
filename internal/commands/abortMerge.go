bc6fffc12955b2020e7afce9f042d2f13c20f977
Petroviiic <108690656+Petroviiic@users.noreply.github.com>
2026-05-18 00:10:12 +0200

    Abort merge: restore working tree and index
    Add full abort-merge logic: check for MERGE_HEAD existence, verify current branch is not detached, and load the branch's last commit. Rebuild an index from that commit's tree, restore working directory files and index to that state, save the index, and delete the MERGE_HEAD. Return errors on failures and print status messages.

--- internal/commands/abortMerge.go
+++ internal/commands/abortMerge.go
@@ -10,7 +10,49 @@ func AbortMerge(repo *core.Repository) error {
 	fmt.Println("aborting merge")
 
 	//check if merge head exists
-	//remove merge head
+	if _, exists := repo.MergeHEADExists(); !exists {
+		return fmt.Errorf("fatal: There is no merge to abort") //promijeni u mainu da ispise fatal a ne error :)
+	}
+
+	currentBranch, isDetached := repo.GetCurrentBranch()
+
+	if isDetached {
+		return fmt.Errorf("branch detached. something went terribly wrong!!!")
+	}
+
+	// RestoreWorkingDirectoryFiles()
+	lastOursCommitHash := repo.GetBranchCommit(currentBranch)
+	obj, err := repo.LoadObject(lastOursCommitHash)
+	if err != nil {
+		return err
+	}
+
+	lastOursCommit := obj.(*core.Commit)
+	lastOursIndex := map[string]core.IndexEntry{}
+	if err := core.GetFilesFromTreeHash(lastOursCommit.TreeHash, repo, "", lastOursIndex); err != nil {
+		return err
+	}
+
+	index, err := repo.LoadIndex()
+
+	if err != nil {
+		return err
+	}
+
 	//restore working dir and index to head.lastcommit
+	if err := RestoreWorkingDirectoryFiles(lastOursIndex, index, "", repo); err != nil {
+		return err
+	}
+
+	if err := repo.SaveIndex(lastOursIndex); err != nil {
+		return err
+	}
+
+	//remove merge head
+	if err := repo.DeleteMergeHEAD(); err != nil {
+		return err
+	}
+
+	fmt.Println("merge sucessfully aborted")
 	return nil
 }
